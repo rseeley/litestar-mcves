6d98f13979c104f0424ad07912b00f5c0554251a
Ryan Seeley <ryseeley@gmail.com>
2023-09-27 08:38:03 -0500

    Add example of schema not showing camel keys

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 """Minimal Litestar application."""
 from __future__ import annotations
 
-from asyncio import sleep
-from typing import Any, Dict
+from pydantic import BaseModel as _BaseModel
+from pydantic import ConfigDict, Field
+from pydash.strings import camel_case
 
 from litestar import Litestar, get
 
-__all__ = ("async_hello_world", "sync_hello_world")
+__all__ = ("async_hello_world",)
+
+
+class BaseModel(_BaseModel):
+    model_config = ConfigDict(
+        validate_assignment=True,
+        from_attributes=True,
+        use_enum_values=True,
+        arbitrary_types_allowed=True,
+    )
+
+
+class CamelizedBaseModel(BaseModel):
+    model_config = ConfigDict(populate_by_name=True, alias_generator=camel_case)
+
+
+class HelloWorld(CamelizedBaseModel):
+    some_value: str
+    other_value: str = Field(..., alias="otherValueAlias")
 
 
 @get("/async")
-async def async_hello_world() -> Dict[str, Any]:  # noqa: UP006
+async def async_hello_world() -> HelloWorld:
     """Route Handler that outputs hello world."""
-    await sleep(0.1)
-    return {"hello": "world"}
+    return HelloWorld(some_value="Hello World", other_value="Hello World")
 
 
-@get("/sync", sync_to_thread=False)
-def sync_hello_world() -> Dict[str, Any]:  # noqa: UP006
-    """Route Handler that outputs hello world."""
-    return {"hello": "world"}
-
-
-app = Litestar(route_handlers=[sync_hello_world, async_hello_world])
+app = Litestar(
+    route_handlers=[async_hello_world],
+    type_encoders={_BaseModel: lambda x: x.model_dump(by_alias=True)},
+)
